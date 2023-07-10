7e6f76b02b77cdf1b2f03c260249e7983ec4a312
11 1 <yehforlivelihood@gmail.com>
2023-07-10 10:06:24 +0800

    Update

--- PushGit.py
+++ PushGit.py
@@ -14,7 +14,7 @@ if is_hexo_generate:
 
 # 是否 hexo deploy
 is_hexo_deploy = input(">>> Is hexo deploy? (y/n) : ")
-if is_hexo_deploy == "y" or is_hexo_deploy == "Y":
+if is_hexo_deploy == "y" or is_hexo_deploy == "Y" or is_hexo_deploy == "yes" or is_hexo_deploy == "Yes" or is_hexo_deploy == "YES":
     # hexo deploy
     hexo_deploy = "hexo deploy"
     print(">>> " + hexo_deploy)
